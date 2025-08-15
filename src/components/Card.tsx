960c13eb1aacd8edbec343479aea25493968a768
Hannes Leonhartsberger <hannes.leonhartsberger@gmail.com>
2025-08-16 00:03:18 +0200

    Added card navigation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import {useNavigate} from "react-router";
+
 interface IProps {
     banner: string,
     title: string,
@@ -6,8 +8,10 @@ interface IProps {
 }
 
 export default ({banner, title, description, href}: IProps) => {
+    let navigate = useNavigate();
+
     return <>
-        <button className="border-solid" onClick={() => {}}>
+        <button className="border-solid" onClick={() => {navigate(href)}}>
             <img src={banner} alt={banner} className="aspect-4/3 object-cover w-full rounded-t-[8px]"/>
             <h2>{title}</h2>
             <p>{description}</p>
